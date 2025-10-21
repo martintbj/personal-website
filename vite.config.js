839528acb3a70a42c73d056ecb8fcab5f953e3c8
mtb <jtrunal1015@gmail.com>
2025-10-20 23:03:56 -0500

    delete

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-website/', // Update this to match your GitHub repo name
+  base: '/personal-website/',
 })
